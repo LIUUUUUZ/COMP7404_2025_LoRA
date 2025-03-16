c45972d806089b38a504a027da444012e5135f7e
boyetian <boyetian@connect.hku.hk>
2025-03-16 16:15:14 +0800

    ag_news
    增加了ag_news数据集，修改了加载数据的方式（加载的时候不删除标签列）

--- parser.py
+++ parser.py
@@ -1,11 +1,11 @@
 import argparse
 
-def get_parser():
 
+def get_parser():
     parser = argparse.ArgumentParser(description="COMP_7404_2025_spring_LoRA项目")
-    
+
     # 数据集部分（若不完善请修改删减增补）
-    parser.add_argument("--dataset", type=str, choices=["snli", "stsb"], default="snli",
+    parser.add_argument("--dataset", type=str, choices=["snli", "stsb", "ag_news"], default="snli",
                         help="数据集名称")
     parser.add_argument("--max_seq_length", type=int, default=128,
                         help="最大序列长度")
@@ -13,7 +13,7 @@ def get_parser():
                         help="训练Batch大小")
     parser.add_argument("--eval_batch_size", type=int, default=64,
                         help="评估Batch大小")
-    
+
     # 模型基础部分（若不完善请修改删减增补）
     parser.add_argument("--model_name", type=str, default="roberta-base",
                         help="底层模型名称")
@@ -31,12 +31,12 @@ def get_parser():
                         help="在反向传播前累积的更新步骤数, 本次实验中不考虑")
     parser.add_argument("--seed", type=int, default=42,
                         help="随机种子")
-    
+
     parser.add_argument("--time_count", action="store_true",
                         help="是否计时")
-    parser.add_argument("--mode", type=str, choices=["train", "eval"], default="train",
+    parser.add_argument("--mode", type=str, choices=["train", "eval", "ag_news"], default="train",
                         help="选择训练或评估模式")
-    
+
     # LoRA微调参数（若不完善请修改删减增补）
     parser.add_argument("--lora_rank", type=int, default=3,
                         help="LoRA层Rank的大小")
@@ -46,30 +46,29 @@ def get_parser():
                         help="LoRA层Dropout概率, 本次实验中不考虑, 默认为0.1")
     parser.add_argument("--test_iters", type=int, default=100,
                         help="测试预测时的iter数")
-    
-    #使用方法为 python xxxx.py --lora_target q v o,必须要有至少一个, 平时默认全部使用
-    parser.add_argument("--lora_target", type=str, nargs='+', 
+    parser.add_argument("--compare_ranks", action="store_true",
+                        help="是否对比不同 LoRA rank 的影响")
+
+    # 使用方法为 python xxxx.py --lora_target q v o,必须要有至少一个, 平时默认全部使用
+    parser.add_argument("--lora_target", type=str, nargs='+',
                         default=["q", "k", "v", "o"],
                         help="LoRA运用的attention目标矩阵的组合")
-    
-    # Adapter线性层微调参数（若不完善请修改删减增补）
-    # 暂无，因为此程序中adapter层不需要更改参数设置，可以直接写在程序中，若需要可以后续添加
-    
-    
 
-    
+    # Adapter配置参数
+    parser.add_argument("--adapter", type=str, choices=["adapter_config_03M", "adapter_config_09M", "adapter_config"],
+                        default="adapter_config", help="adapter参数")
+
     # 模型储存相关（若不完善请修改删减增补）
     parser.add_argument("--output_dir", type=str, default="./outputs",
                         help="保存输出的目录")
     parser.add_argument("--save_model", action="store_true",
                         help="是否保存训练好的模型")
-    
 
     parser.add_argument("--output_matrices", action="store_true",
                         help="是否输出预训练矩阵和LoRA微调后的矩阵")
 
-    
     return parser
+
 
 def parse_args():
     parser = get_parser()
